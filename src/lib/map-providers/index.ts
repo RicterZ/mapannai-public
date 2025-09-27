ab0570784f991d1688011c4f0bfc3fae8634fb3d
ricterz <ricterzheng@gmail.com>
2025-09-27 16:36:37 +0800

    add amap support

--- src/lib/map-providers/index.ts
+++ src/lib/map-providers/index.ts
@@ -1,2 +1,3 @@
 export { MapboxProvider } from './mapbox-provider'
+export { AmapProvider } from './amap-provider'
 export { MapProviderFactoryImpl, mapProviderFactory } from './map-provider-factory'
